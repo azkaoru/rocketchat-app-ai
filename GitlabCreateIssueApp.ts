118e6da7ea99eeec32728d35f2b4316de940f816
kaoru.watanabe <kaoru.watanabe.xn@hco.ntt.co.jp>
2025-08-15 14:07:03 +0900

    update

--- GitlabCreateIssueApp.ts
+++ GitlabCreateIssueApp.ts
@@ -109,7 +109,7 @@ export class GitlabCreateIssueApp extends App implements IPostMessageSent {
             title: issueTitle,
             description: issueDescription,
             labels: [issueLabel1, issueLabel2],
-            assignee_username: ["ai_qwen"],
+            assignee_ids: [9],
         };
 
         const request = {
